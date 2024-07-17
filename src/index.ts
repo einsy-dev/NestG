6e6cc8e4d75d49db0f8541292071bea00aa78806
Edgard <eins.yeager@gmail.com>
2024-07-17 14:15:50 +0500

    update def args

--- src/index.ts
+++ src/index.ts
@@ -7,23 +7,27 @@ const program = new Command();
 program
   .name("nestg")
   .description("generate elements for Nest.js")
-  .version("1.0.0")
+  .version("1.1.1")
   .action(async () => {
     const { elements } = await getInput();
     const args = process.argv.slice(2).join(" ");
+    const defArgs = "--no-spec";
 
     for (const element of elements) {
-      await exec(`nest g ${element} ${args}`, (err, stdout, stderr) => {
-        if (err) {
-          console.log(`error: ${err.message}`);
-          return;
+      await exec(
+        `nest g ${element} ${args} ${defArgs}`,
+        (err, stdout, stderr) => {
+          if (err) {
+            console.log(`error: ${err.message}`);
+            return;
+          }
+          if (stderr) {
+            console.log(`stderr: ${stderr}`);
+            return;
+          }
+          console.log(`stdout: ${stdout}`);
         }
-        if (stderr) {
-          console.log(`stderr: ${stderr}`);
-          return;
-        }
-        console.log(`stdout: ${stdout}`);
-      });
+      );
     }
   })
   .parse();
